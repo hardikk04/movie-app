5e673fffbbe93e66adacf1993a34982d9cdbb84f
Hardik Sisodiya <hardik04062003@gmail.com>
2025-11-22 19:05:28 +0530

    init react native app

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      
+      className="flex-1 justify-center items-center"
+    >
+      <Text className="text-xl font-bold text-light-100">Welcome</Text>
+    </View>
+  );
+}
